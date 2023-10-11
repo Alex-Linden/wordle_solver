fddde8694457e83b344e2f04a1bddf568cd5933c
Alex Linden <alextl2004@gmail.com>
2023-10-11 16:24:04 -0700

    Score class

--- word_solver.py
+++ word_solver.py
@@ -1,17 +1,25 @@
 """words will be in dict format
     {letter: (blank, yellow, green)}
-"""
+    can't use dict because words with repeat letters will break
+    teeth => {'t':blank, e:green}"""
+from enum import Enum
 
 
-def filtered_words(words, guess):
+class Score(Enum):
+    Blank = 0
+    Yellow = 1
+    Green = 2
+
+
+def filtered_words(words, guess, guess_score):
     out = []
     for word in words:
-        for i, (ltr, score) in enumerate(guess.items()):
-            if score == "blank" and ltr in word:
+        for i, (ltr, score) in enumerate(zip(guess, guess_score)):
+            if score == Score.Blank and ltr in word:
                 break
-            elif score == "green" and word[i] != ltr:
+            elif score == Score.Green and word[i] != ltr:
                 break
-            elif score == "yellow" and (word[i] == ltr or ltr not in word):
+            elif score == Score.Yellow and (word[i] == ltr or ltr not in word):
                 break
         else:
             out.append(word)
@@ -19,12 +27,12 @@ def filtered_words(words, guess):
 
 
 def score_guess(guess, potential_answer):
-    out = {}
+    out = []
     for i, ltr in enumerate(guess):
         if ltr == potential_answer[i]:
-            out[ltr] = "green"
+            out.append(Score.Green)
         elif ltr in potential_answer:
-            out[ltr] = "yellow"
+            out.append(Score.Yellow)
         else:
-            out[ltr] = "blank"
-    return out
+            out.append(Score.Blank)
+    return tuple(out)
